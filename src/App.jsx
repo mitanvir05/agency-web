10974716c2c0024289018304be76d5973f4eb325
mitanvir05 <tanvir.cse05@gmail.com>
2024-04-20 00:33:07 +0600

    about added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import About from './components/About'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Services from './components/Services'
@@ -12,6 +13,7 @@ function App() {
    <Navbar></Navbar>
    <Home></Home>
    <Services></Services>
+   <About></About>
     </>
   )
 }
